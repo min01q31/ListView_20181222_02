d4ae6914c417874d6ab77ce399f8b89a585806b3
wosad878 <wosad878@naver.com>
2018-12-22 12:41:57 +0900

    멤버 어댑터 골격 완성

---
+++ app/src/main/java/com/iu/listview_20181222_02/dapters/MemberAdapter.java
@@ -0,0 +1,40 @@
+package com.iu.listview_20181222_02.dapters;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.iu.listview_20181222_02.R;
+import com.iu.listview_20181222_02.datas.Member;
+
+import java.util.List;
+
+public class MemberAdapter extends ArrayAdapter<Member> {
+
+    Context mContext = null;
+    List<Member> mList = null;
+    LayoutInflater inf = null;
+
+
+    public MemberAdapter(Context context,List<Member> list){
+        super(context, R.layout.member_list_item, list);
+
+        this.mContext = context;
+        this.mList = list;
+        this.inf = LayoutInflater.from(mContext);
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View row = convertView;
+        if(row == null){
+            row = inf.inflate(R.layout.member_list_item,null);
+        }
+
+        return row;
+    }
+}
